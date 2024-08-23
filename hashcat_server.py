321db810307c444fe8ae6e09a478d0a903150fd9
Norbert Domcsek <liquidmind@me.com>
2024-08-23 08:09:23 +0100

    Update hashcat_server.py

--- hashcat_server.py
+++ hashcat_server.py
@@ -6,7 +6,7 @@ import pwnagotchi.plugins as plugins
 
 class HashcatServer(plugins.Plugin):
     __author__ = 'liquidmind@me.com'
-    __version__ = '1.0.3'
+    __version__ = '1.0.4'
     __license__ = 'GPL3'
     __description__ = 'Converts pcap files to .22000 format and uploads them to a server when internet is available. Also checks and displays available jobs.'
 
